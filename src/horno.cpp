f458f31dd26cc72d72e098c96ed3ba6b0302fc37
Jeison <jeisonhzm@gmail.com>
2025-10-06 02:14:17 -0300

    feat: add BT serial command interface and remove finite state machine

--- src/horno.cpp
+++ src/horno.cpp
@@ -13,10 +13,6 @@
 #define UPPER tmpSet + INTERVAL
 #define LOWER tmpSet - INTERVAL
 
-#define LOW_STATE 0
-#define SET_STATE 1
-#define HIGH_STATE 2
-
 // == Serial BT ==
 
 const byte rxPin = 2;
@@ -35,25 +31,23 @@ const byte ledHigh = 10;
 
 // == Global Variables ==
 
-char RX;
-
 bool start = false;
 bool stop = true;
 
-int tmp = 20;
-int tmpSet = 25;
+int tmp = 51;
+int tmpSet = 40;
 
 int time = 0;
-int timeSet = 60;
+int timeSet = 10;
 bool start_timer = false;
+int timer_counter = 0;
 
-float adc = 0;
-
-// == State Machine ==
-
-int state = LOW_STATE;
+int ADC0 = A5;
+float adc_samples = 0;
+int adc_counter = 0;
 
 // put function declarations here:
+void serialRead();
 
 void setup()
 {
@@ -72,12 +66,22 @@ void setup()
 void loop()
 {
     // put your main code here, to run repeatedly:
-    if (stop)
+
+    // == Serial ==
+
+    serialRead();
+
+    // == Main System ==
+
+    if (stop || !start)
     {
-        // == Reset State ==
+        // == Reset ==
 
         time = timeSet;
         start_timer = false;
+        adc_counter = 0;
+        adc_samples = 0;
+        timer_counter = 0;
 
         digitalWrite(LED_START, LOW);
         digitalWrite(LED_LOW, LOW);
@@ -87,69 +91,170 @@ void loop()
 
         // == Stop Blink ==
 
+        digitalWrite(LED_STOP, LOW);
+        delay(250);
         digitalWrite(LED_STOP, HIGH);
         delay(250);
-        digitalWrite(LED_STOP, LOW);
     }
-    else
+    else if (start)
     {
-        // Stop LED Off
+        // LEDs
         digitalWrite(LED_STOP, LOW);
+        digitalWrite(LED_START, HIGH);
 
-        if (BT.available())
+        // Sample ADC
+        adc_samples = adc_samples + analogRead(ADC0);
+        adc_counter++;
+
+        if (adc_counter == SAMPLES)
         {
-            RX = BT.read();
-            Serial.write(BT.read()); // print
-            switch (RX)
+            adc_counter = 0;
+
+            tmp = int(trunc((((((adc_samples / SAMPLES) * 5.0) / 1023.0)) - 0.12) * 100)); // 0.12 V offset of LM358
+            BT.print("Temperature = ");
+            BT.println(tmp);
+
+            adc_samples = 0;
+
+            if (tmp < LOWER)
             {
-            case 'P':
-                BT.println("Start Sent");
-                start = !digitalRead(start);
-                break;
-            case 'S':
-                BT.println("Stop Sent");
-                stop = !digitalRead(stop);
-                break;
+                digitalWrite(LED_LOW, HIGH);
+                digitalWrite(LED_SET, LOW);
+                digitalWrite(LED_HIGH, LOW);
+                digitalWrite(HORNO, HIGH);
             }
-        }
-        // == State Machine ==
-        if (start)
-        {
-
-            // ADD ADC READ WITH SAMPLES
-
-            switch (state)
+            else if (tmp >= LOWER || tmp <= UPPER)
             {
-            case LOW_STATE:
-                if (tmp >= LOWER)
-                    state = SET_STATE;
-                break;
-            case SET_STATE:
                 start_timer = true;
-                if (tmp < LOWER)
-                    state = LOW_STATE;
-                else if (tmp > UPPER)
-                    state = HIGH_STATE;
-                break;
-            case HIGH_STATE:
-                if (tmp <= UPPER)
-                    state = SET_STATE;
-                break;
+                digitalWrite(LED_LOW, LOW);
+                digitalWrite(LED_SET, HIGH);
+                digitalWrite(LED_HIGH, LOW);
+                digitalWrite(HORNO, HIGH);
             }
-            // output logic
-            digitalWrite(LED_START, !stop ? HIGH : LOW);
-            digitalWrite(LED_LOW, (tmp <= LOWER && state == LOW_STATE) ? HIGH : LOW);
-            digitalWrite(LED_SET, (!(tmp > UPPER) && !(tmp < LOWER) && state == SET_STATE) ? HIGH : LOW);
-            digitalWrite(LED_HIGH, (tmp >= UPPER && state == HIGH_STATE) ? HIGH : LOW);
-            digitalWrite(HORNO, (tmp <= UPPER && state == SET_STATE) ? HIGH : LOW);
-            // timer
-            if (start_timer)
+            if (tmp > UPPER)
             {
-                time = time - 1;
-                delay(1000);
+                start_timer = true;
+                digitalWrite(LED_LOW, LOW);
+                digitalWrite(LED_SET, LOW);
+                digitalWrite(LED_HIGH, HIGH);
+                digitalWrite(HORNO, LOW);
             }
+
+            // == Timer ==
+
+            if (start_timer && timer_counter == 4)
+            {
+                timer_counter = 0;
+                time = time - 1;
+                BT.print("Time = ");
+                BT.println(time);
+                if (time == 0)
+                {
+                    stop = true;
+                    start = false;
+                    BT.println("TIMEOUT!");
+                    BT.println("System Shutdown");
+                }
+            }
+
+            timer_counter++;
+            delay(250);
         }
     }
 }
 
 // put function definitions here:
+void serialRead()
+{
+    if (BT.available())
+    {
+        String input;
+        String command;
+        String value;
+
+        input = Serial.readStringUntil('\n'); // Read the full line until newline
+
+        input.trim(); // Remove any leading/trailing whitespace
+
+        if (input.length() == 0)
+            return;
+
+        int equalIndex = input.indexOf('=');
+        if (equalIndex != -1)
+        {
+            command = input.substring(0, equalIndex);
+            value = input.substring(equalIndex + 1);
+        }
+        else
+            command = input;
+
+        // == Terminal ==
+
+        if (command == "START")
+        {
+            start = !start;
+            BT.print("Start : ");
+            BT.println(start ? "ON" : "OFF");
+        }
+        else if (command == "STOP")
+        {
+            stop = !stop;
+            BT.print("Stop : ");
+            BT.println(stop ? "ON" : "OFF");
+        }
+        else if (command == "TMP_SET")
+        {
+            if (equalIndex != -1)
+            {
+                tmpSet = value.toInt();
+                Serial.print("Temperature set to = ");
+                Serial.println(tmpSet);
+            }
+            else
+            {
+                Serial.println("Invalid format! Use TMP_SET=XXX");
+            }
+        }
+        else if (command == "TIME_SET")
+        {
+            if (equalIndex != -1)
+            {
+                timeSet = value.toInt();
+                Serial.print("Time set to = ");
+                Serial.println(timeSet);
+            }
+            else
+            {
+                Serial.println("Invalid format! Use TIME_SET=XXX");
+            }
+        }
+        else if (command == "SHOW")
+        {
+            BT.println("--------------------");
+            BT.print("Start : ");
+            BT.println(start ? "ON" : "OFF");
+            BT.print("Stop : ");
+            BT.println(stop ? "ON" : "OFF");
+            BT.print("Temperature set = ");
+            BT.println(tmpSet);
+            BT.print("Time set = ");
+            BT.println(timeSet);
+            BT.println("--------------------");
+        }
+        else if (command == "HELP")
+        {
+            BT.println("--------------------");
+            Serial.println("SHOW        // Show system state");
+            Serial.println("START       // System start (latched)");
+            Serial.println("STOP        // System shutdown (latched)");
+            Serial.println("TMP_SET     // Temperature set point (TMP_SET=XXX)");
+            Serial.println("TIME_SET    // Countdown time to shutdown (TIME_SET=XXX)");
+            BT.println("--------------------");
+        }
+        else
+        {
+            Serial.println("Command not found! Use HELP to show available commands");
+        }
+    }
+    return;
+}
